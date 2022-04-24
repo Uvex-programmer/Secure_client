721f0d57849842c52e9cb6d60c31ba13a50d5179
Uvex-programmer <77095105+Uvex-programmer@users.noreply.github.com>
2022-04-24 11:06:09 +0200

    feature: Added who am i method, to keep user logged in with jwt

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,31 @@
 import AllRoutes from './AllRoutes/AllRoutes'
-import GroupBar from './GroupBar/GroupBar'
-import { Grid, Typography } from '@mui/material'
+import GroupBar from '../GroupBar/GroupBar'
+import { Grid } from '@mui/material'
 import Navbar from '../Navbar/Navbar'
+import { useEffect } from 'react'
+import { useAuth } from '../../store/AuthContext'
 
 function App() {
+  const auth = useAuth()
+  useEffect(() => {
+    if (auth.user) return
+    auth.whoAmI()
+  }, [auth.user])
+
   return (
     <Grid container height='100%'>
       <Grid item minWidth={'100%'} maxHeight={'62px'}>
         <Navbar />
       </Grid>
-      <Grid item md={2} minWidth={300} bgcolor='gray' minHeight='100%'>
+      <Grid item md={2} bgcolor='gray' minHeight='100%'>
         <GroupBar />
       </Grid>
-      <Grid item>hej</Grid>
-      <Grid item>hej</Grid>
+      <Grid item md={8}>
+        <AllRoutes />
+      </Grid>
+      <Grid item md={2} bgcolor='gray' minHeight='100%'>
+        <GroupBar />
+      </Grid>
     </Grid>
   )
 }

c2a0d36c89f58159fa5f406219cbf34f9b6a0b28
Uvex-programmer <77095105+Uvex-programmer@users.noreply.github.com>
2022-04-24 11:09:35 +0200

    Feature: Added protectedRoute component that will protect the routes that we need jwt for

--- src/components/App/AllRoutes/AllRoutes.js
+++ src/components/App/AllRoutes/AllRoutes.js
@@ -1,22 +1,34 @@
 import { Route, Routes } from 'react-router-dom'
-import { Divider, Loader } from 'semantic-ui-react'
 import Login from '../../../pages/LoginPage/login'
-import NoMatchRoute from '../noMatchRoute'
+import Signup from '../../../pages/SignupPage/Signup'
+import HomePage from '../../../pages/HomePage/HomePage'
+import NoMatchRoute from '../../../pages/noMatchRoute'
+import GroupPage from '../../../pages/GroupPage/GroupPage'
+import ProtectedRoute from './ProtectedRoute'
 
 const AllRoutes = () => {
-  if (false) {
-    return (
-      <>
-        <Divider hidden className='small' />
-        <Loader active inline='centered' className='transparent'></Loader>
-        <Divider hidden className='small' />
-      </>
-    )
-  }
-
   return (
     <Routes>
-      <Route exact path={process.env.REACT_APP_HOME_URL} element={<Login />} />
+      <Route exact path={process.env.REACT_APP_LOGIN_URL} element={<Login />} />
+      <Route
+        exact
+        path={process.env.REACT_APP_SIGNUP_URL}
+        element={<Signup />}
+      />
+      <Route
+        exact
+        path={process.env.REACT_APP_HOME_URL}
+        element={<HomePage />}
+      />
+      <Route
+        exact
+        path={process.env.REACT_APP_GROUP_URL}
+        element={
+          <ProtectedRoute>
+            <GroupPage />
+          </ProtectedRoute>
+        }
+      />
       <Route path='*' element={<NoMatchRoute />} />
     </Routes>
   )
---
+++ src/components/App/AllRoutes/ProtectedRoute.js
@@ -0,0 +1,29 @@
+import { Navigate } from 'react-router-dom'
+import { useAuth } from '../../../store/AuthContext'
+import { useState, useEffect } from 'react'
+
+const ProtectedRoute = ({ children }) => {
+  const auth = useAuth()
+  const [loading, setLoading] = useState(true)
+  const [authenticated, setAuthenticated] = useState(false)
+
+  useEffect(() => {
+    const authenticate = () => {
+      const authenticated = auth.authenticate()
+      setAuthenticated(authenticated)
+      setLoading(false)
+    }
+    authenticate()
+  }, [])
+
+  if (loading) {
+    return <h1>KOLLAR SÅ DU INTE ÄR EN HACKER</h1>
+  }
+
+  if (!authenticated) {
+    return <Navigate to='/' replace />
+  }
+  return children
+}
+
+export default ProtectedRoute

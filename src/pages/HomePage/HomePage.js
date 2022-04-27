f82a99aadeed333d9e52a9ab3f31162d105d9c0e
Martin Isaksen <izaxen@telia.com>
2022-04-27 22:28:25 +0200

    todos

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const HomePage = () => {
-  return <div>Välkommen till en hemsida</div>
+  return <div>
+    Att göra:<br></br>
+    Skapa huvudadmin<br></br>
+    Låsa grupper som private till inloggade<br></br>
+    Lösa access för poster<br></br>
+
+  </div>
 }
 
 export default HomePage

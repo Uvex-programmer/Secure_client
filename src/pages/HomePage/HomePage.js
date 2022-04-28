b914f93207ac2ac85740db0d8a7be3e7fd9f663f
Uvex-programmer <77095105+Uvex-programmer@users.noreply.github.com>
2022-04-28 21:31:52 +0200

    fix

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const HomePage = () => {
-  return <div>
-    Att göra:<br></br>
-    Skapa huvudadmin<br></br>
-    Låsa grupper som private till inloggade<br></br>
-    Lösa access för poster<br></br>
-
-  </div>
+  return (
+    <div>
+      Att göra:<br></br>
+      Skapa huvudadmin<br></br>
+      Lösa access för poster<br></br>
+    </div>
+  )
 }
 
 export default HomePage
